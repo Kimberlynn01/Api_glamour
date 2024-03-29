8f267ad2abc2b1e9330826ca9d6ee20fd24b0a5a
danudiraja <danudiraja@gmail.com>
2024-03-29 13:17:08 +0700

    fix: fix cant access server

--- productApi.js
+++ productApi.js
@@ -70,7 +70,81 @@ app.get("/products", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server berjalan di port ${PORT}`);
-});
+module.exports = app;
+
+// const express = require("express");
+// const admin = require("firebase-admin");
+
+// const serviceAccount = require("./api-glamour-app-firebase-adminsdk-wf5c2-9fbcd7e825.json");
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://api-glamour-app-default-rtdb.asia-southeast1.firebasedatabase.app/",
+// });
+
+// const app = express();
+
+// app.use(express.json());
+
+// app.post("/products", async (req, res) => {
+//   try {
+//     const { title, price, description, category, images } = req.body;
+
+//     if (!title || !price || !description || !category || !images) {
+//       return res.status(400).json({ error: "Data produk tidak lengkap" });
+//     }
+
+//     const productsRef = admin.database().ref("products");
+
+//     const snapshot = await productsRef.once("value");
+//     const productList = snapshot.val();
+
+//     const nextId = Object.keys(productList || {}).length + 1;
+
+//     const newProductRef = productsRef.child(nextId.toString());
+
+//     await newProductRef.set({
+//       id: nextId,
+//       title: title,
+//       price: price,
+//       description: description,
+//       category: category,
+//       images: images,
+//     });
+
+//     res.status(201).json({
+//       id: nextId,
+//       title: title,
+//       price: price,
+//       description: description,
+//       category: category,
+//       images: images,
+//     });
+//   } catch (error) {
+//     console.error("Error:", error);
+//     res.status(500).json({ error: "Terjadi kesalahan saat menambahkan produk" });
+//   }
+// });
+
+// app.get("/products", async (req, res) => {
+//   try {
+//     const productsRef = admin.database().ref("products");
+
+//     const snapshot = await productsRef.once("value");
+//     const productList = snapshot.val();
+
+//     const productsArray = Object.keys(productList || {}).map((key) => ({
+//       id: key,
+//       ...productList[key],
+//     }));
+
+//     res.status(200).json(productsArray);
+//   } catch (error) {
+//     console.error("Error:", error);
+//     res.status(500).json({ error: "Terjadi kesalahan saat mengambil data produk" });
+//   }
+// });
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server berjalan di port ${PORT}`);
+// });
--- server.js
+++ server.js
@@ -1,25 +1,23 @@
-// Import library yang dibutuhkan
+const { createServer } = require("http");
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
 const admin = require("firebase-admin");
 
-// Middleware untuk mengizinkan JSON body parsing
 app.use(express.json());
 
-// Endpoint contoh
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("Backend API EndPoint By Danu");
 });
 
-// Jalankan server
-app.listen(PORT, () => {
-  console.log(`Server berjalan di port ${PORT}`);
-});
-
-const serviceAccount = require("api-glamour-app-firebase-adminsdk-wf5c2-9fbcd7e825.json");
+const serviceAccount = require("./api-glamour-app-firebase-adminsdk-wf5c2-9fbcd7e825.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://api-glamour-app-default-rtdb.asia-southeast1.firebasedatabase.app/",
+});
+
+const server = createServer(app);
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Server berjalan di port ${process.env.PORT || 3000}`);
 });
